6de2e57569ca1231596eeaf462bb33b44548848b
JCSama16 <jeancarlos.samaniegolanda.23@campusfp.es>
2024-02-02 09:19:11 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hito1_2ºTri',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
